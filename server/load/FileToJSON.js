62f026de60cc1d0750854cb99aafda26c9e7a70b
Jason David Peters <iamjasonpeters@live.co.uk>
2020-12-10 10:49:15 -0800

    Added functionality. Placeholder logo, beginnings of a nav bar (will need to work on routing in the future). One way and return radio buttons which will interact with the return date.

---
+++ server/load/FileToJSON.js
@@ -0,0 +1,6 @@
+/*
+	PDF To JSON conversion
+*/
+var fs = require('fs');
+
+// load the file into a stream
--- website/js/autocomplete.js
+++ website/js/autocomplete.js
@@ -3,23 +3,45 @@ var maxSuggestions = 5; // Maximum suggestions that will be displayed in the aut
 var airports = airports; // Reference external array so we later don't go looking for it.
 var validAirports =	new Set(); // Using a set object as to remove the need to check for duplicate values
 
+function removeQuery() {
+	var parent = this.parentElement;
+	setTimeout(function() {
+		/*
+			Check for children of parent node, if they are equal to any of the airport nodes
+			remove those children.
+		*/
+		for (var i = 0; i < airports.length; i++) {
+			// go through children of parent and remove airport objects
+			var e = document.getElementById(airports[i].name);
+			if (e != null)
+				parent.removeChild(e);
+		}
+	}, 150);
+}
+
 function autoComplete() {
-	var input = searchBox.value;
+	var input = this.value;
 	
 	for (var i = 0; i < airports.length; i++) {
-		if (((airports[i].name).toLowerCase()).includes((input.toLowerCase()))) {
-			if (validAirports.length >= maxSuggestions) {
-				return;
+
+		if ((((airports[i].name).toLowerCase()).includes((input.toLowerCase()))
+			|| ((airports[i].code).toLowerCase()).includes((input.toLowerCase()))
+			|| ((airports[i].state).toLowerCase()).includes((input.toLowerCase())))
+			&& input.length > 3) {
+			if (validAirports.size >= maxSuggestions) {
+				break;
 			} else {
 				// check if the array contains the 
 				validAirports.add(airports[i]);
 			}
 			// add the result to an array
 		} else {
-			if (validAirports.has(airports[i]))validAirports.delete(airports[i]);
+			if (validAirports.has(airports[i])) {
+				validAirports.delete(airports[i]);
+			}
 			var e = document.getElementById(airports[i].name);
 			if (e != null)
-			searchBox.parentElement.removeChild(e);
+			this.parentElement.removeChild(e);
 			
 		}
 	}
@@ -32,9 +54,9 @@ function autoComplete() {
 		suggestion.style.position = "absolute";
 		suggestion.style.color="black";
 		suggestion.style.backgroundColor="white";
-		suggestion.style.borderColor="black";
+		suggestion.style.borderColor="gray";
 		suggestion.style.borderStyle="solid";
-		suggestion.innerHTML = arrayConvert[j].name;
+		suggestion.innerHTML = arrayConvert[j].name + ", " + arrayConvert[j].state + " (" + arrayConvert[j].code+")";
 		suggestion.style.height="38px";
 		suggestion.style.width="200px";
 		suggestion.style.backgroundColor="0";
@@ -42,9 +64,13 @@ function autoComplete() {
 		suggestion.style.bottom="0";
 		suggestion.style.left="0";
 		suggestion.style.right="0";
+		
+		suggestion.addEventListener("click", event => {
+			this.value = suggestion.innerHTML;
+		});
 	//	if (!document.body.contains(document.getElementById(suggestion.id)))
 		//console.log(searchBox.parentElement);
-		var parent = searchBox.parentElement;
+		var parent = this.parentElement;
 		if (document.getElementById(arrayConvert[j].name) == null) {
 			parent.appendChild(suggestion);
 			// if it doesn't already exist then we can append it
@@ -53,15 +79,11 @@ function autoComplete() {
 			
 		var yPos = window.getComputedStyle(document.getElementById(arrayConvert[j].name)).getPropertyValue('top');
 		
-		console.log("position: " + j + " current y position:" + yPos + " " + (j+1)*38);
 		if (((j+1)*38) < parseInt(yPos)) {
-			console.log("updating");
 			document.getElementById(arrayConvert[j].name).style.top="calc("+(j+1)+"*38px)";
 
 		}
-		// if get top attribute > current calculation, reset top attribute
-		
-		// TODO: if an element is removed, take previous position
+		// TODO add an onclick event to each suggestion element, set the value of 'this' input box
 		
 	}
 	
