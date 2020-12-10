2ef737d4ede58b32dda27c29c49819dacbbdee1c
Jason David Peters <iamjasonpeters@live.co.uk>
2020-12-10 05:12:22 -0800

    Began work on auto suggest/auto complete of input text fields based on an array loaded from JSON. Functionality is there now, need to make it more robust and perhaps load the airports into JSON through a pdf or similar file system.

---
+++ server/load/AssetLoader.js
@@ -0,0 +1,34 @@
+var fs = require('fs');	
+var dir;
+var global = require("../util/GlobalVars.js");
+
+	function checkFileExtension(url) {
+		if (url != null && url.includes(".")) {
+			var start = 0;
+			var end = url.indexOf('.');
+			dir = global.webDir+url;
+			return true;
+		} else {
+			return false;
+		}
+		return false;
+	}
+	function loadFile(url) { // Take in a string url, return a readfilestream
+		var file;
+		if (checkFileExtension(url)) { // html is requesting local css in header but cannot find it at /css/...etc because thats not a server directory
+			try {
+				file = fs.readFileSync(dir, null, function(error, data) {
+				if (error)  {
+					console.log('Error loading file');
+					return null; 
+				}
+			}); // we find the directory of the css file and send it back to the html head
+			} catch (err) {
+				console.log('File not found');
+			}
+			return file;
+			//return file; // write the response to the request which is the html header <link>
+		}
+		return null;
+	}
+module.exports={checkFileExtension, loadFile};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
 var http = require('http');
 var PORT = 8080;
-var css = require('./load/Css.js');
-var image = require('./load/Image.js');
+var assets = require('./load/AssetLoader.js');
 var route = require('./Routing.js');
 
 function handleResponse(request, response) {
-	
-	var cssFile = css.loadCss(request.url);
-	if (cssFile != null) {
-		response.write(cssFile);
-		response.end();
-	}
-	if ((imageFile = image.loadImage(request.url)) != null) {
-		response.write(imageFile);
+	console.log('Request: ' + request.url);
+	var file = assets.loadFile(request.url);
+	if (file != null) {
+		response.write(file);
 		response.end();
 	}
 	route.routeClient(request,response);
